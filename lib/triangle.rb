908c12c1d4fc475a798516ac58141736e54c18ea
Zacharia <ndunguzachary24@gmail.com>
2022-08-30 00:30:00 +0300

    commit tree 8255a530d52bb1717d0ee90b3c992dc5f91c7949

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :a , :b, :c;
+
+  def initialize(a, b, c )
+    @a = a 
+    @b = b
+    @c = c
+  end
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+    begin 
+      raise TriangleError
+      rescue TriangleError < StandardError
+        puts error.message
+      end
+  end
+
+
+class Triangle < StandardError
+  def message 
+    "Not a triangle "
+end
+
+end
 end
